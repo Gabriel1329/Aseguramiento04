288201d88681696d891a4667d8354a66d112466c
Gabriel1329 <gabrielpizarro14@gmail.com>
2016-11-20 23:45:48 -0600

    servicio correo

---
+++ ServicioCorreo/ServicioCorreo/Correo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Mail;
+
+namespace ServicioCorreo
+{
+    class Correo
+    {
+
+
+
+        public void EnviarCorreo()
+        {
+            using (SmtpClient cliente = new SmtpClient("smtp.live.com", 25))
+            {
+                cliente.EnableSsl = true;
+                cliente.Credentials = new NetworkCredential("gabriel14_cr@hotmail.com", "contrasena");
+                MailMessage mensaje = new MailMessage("gabriel14_cr@hotmail.com", "gabrielpizarro14@gmail.com", "asuntirijillo", "contenido papu :v");
+                try
+                {
+                    cliente.Send(mensaje);
+                }
+                catch (Exception ex)
+                {
+                }
+            }
+        }
+    }
+}
